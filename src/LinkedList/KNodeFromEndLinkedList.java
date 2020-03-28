b2a0b675d41d549d4da93560020820d25e49a71a
Shivam <shivamsharma0501@gmail.com>
2020-03-28 12:03:55 +0530

    K Node from End in a Linked List

---
+++ src/LinkedList/KNodeFromEndLinkedList.java
@@ -0,0 +1,74 @@
+package LinkedList;
+
+import java.util.Scanner;
+
+class KNodeFromEndLinkedList {
+    static Node head = null;
+    //KNodeFromEndLinkedList() {
+  //  head = null;
+//}
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+        }
+    }
+
+   static void push(int data) {
+        Node temp = new Node(data);
+        temp.next = head;
+        head = temp;
+    }
+
+
+   static void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+     static Node KNodeFromEnd(Node node, int k) {
+          Node p = node;
+          Node q = node;
+          int count;
+        for (count = 1; count < k && p != null; count++)
+            p = p.next;
+        if (p == null)
+            return null;
+        while (p.next != null) {
+            p = p.next;
+            q = q.next;
+        }
+        return q;
+    }
+
+    //Driver Program
+    public static void main(String[] args) {
+      //  KNodeFromEndLinkedList kn = new KNodeFromEndLinkedList();
+       // int k = 3;
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        for (int i = 6; i > 0; i--) {
+            push(i);
+        }
+        printList();
+        head = KNodeFromEnd(head, k);
+        if(head == null)
+        {
+            System.out.println(k +" is Greater than Number of nodes in a Linked List");
+        }
+        System.out.println("\n" + k  + "  Element from End in  Linked list is "+head.data);
+        //System.out.print(" \n"+head.data);
+
+
+    }
+}
+
+
+
+
